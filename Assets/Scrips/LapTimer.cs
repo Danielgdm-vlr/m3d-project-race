46e56539179525f98a056831b69f8e762dfe667c
Danielgdm-vlr <daniel.dany16@yahoo.com>
2022-04-17 18:40:04 +0300

    some changes

---
+++ Assets/Scrips/LapTimer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimer : MonoBehaviour
+{
+
+    private int minutesCounter;
+    private int secondsCounter;
+    private float milisecondsCounter;
+    private string milisecondsDisplay;
+
+    public GameObject minutesBox;
+    public GameObject secondsBox;
+    public GameObject milisecondsBox;
+    
+    void Update()
+    {
+
+        calculateMiliseconds();
+        calculateSeconds();
+        calculateMinutes();       
+    }
+
+    private void calculateMiliseconds(){
+
+        milisecondsCounter += Time.deltaTime * 10;
+        milisecondsDisplay = milisecondsCounter.ToString("F0");
+        milisecondsBox.GetComponent<Text>().text = milisecondsDisplay; 
+    }
+
+    private void calculateSeconds(){
+
+        if(milisecondsCounter >= 10){
+            milisecondsCounter = 0;
+            secondsCounter += 1;
+        }
+
+        if(secondsCounter <= 9){
+            secondsBox.GetComponent<Text>().text ="0" + secondsCounter + ":";
+            return;
+        }
+        secondsBox.GetComponent<Text>().text = secondsCounter + ":";
+    }
+
+    private void calculateMinutes(){
+    
+
+    }
+}
