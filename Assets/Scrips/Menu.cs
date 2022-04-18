f386a507bd9c4317850613b139a862d709234d6b
bucurstefan <bucuradnan@gmail.com>
2022-04-18 15:26:56 +0300

    menu works

--- Assets/Scrips/Menu.cs
+++ Assets/Scrips/Menu.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class Menu : MonoBehaviour
 {
+    public GameObject startButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0)){
+            SceneManager.LoadScene("race_track_lake");
+        }
     }
 }
